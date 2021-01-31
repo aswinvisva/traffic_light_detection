411f3daaca13a0890cee0370cc183ffe3fdb48ce
“Aswin <aswine.ajanthan.visva@edu.uwaterloo.ca>
2021-01-31 12:59:53 -0500

    Cleaned repo

--- detect.py
+++ detect.py
@@ -1,66 +1,64 @@
-import math
-from math import degrees, atan2, pi
-
 import numpy as np
 import cv2 as cv
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans, MiniBatchKMeans
-import matplotlib.path as mplPath
 
 
-def normalize_image(img):
-    V = np.mean(img[:, :, 2])
-    kV = 10
-    stdV = np.std(img[:, :, 2])
-    stdV_m = np.std(img[:, :, 2] - V)
+def arrow_direction(arrowROI):
+    height, width = arrowROI.shape
 
-    img[:, :, 2] = kV * V + stdV * (img[:, :, 2] - V)
-    return img
+    width_cutoff = width // 2
+    s1 = arrowROI[:, :width_cutoff]
+    s2 = arrowROI[:, width_cutoff:]
 
+    left = cv.countNonZero(s1)
+    right = cv.countNonZero(s2)
 
-def weighted_hue(img):
-    green_h = 90
-    yellow_h = 20
-    red_h = 0
-
-    green_w = np.exp(-1 * np.square((img[:, :, 0] - green_h)) / (np.std(np.square((img[:, :, 0] - green_h)))))
-    red_w = np.exp(-1 * np.square((img[:, :, 0] - red_h)) / (np.std(np.square((img[:, :, 0] - red_h)))))
-    yellow_w = np.exp(-1 * np.square((img[:, :, 0] - yellow_h)) / (np.std(np.square((img[:, :, 0] - yellow_h)))))
-    weight = np.maximum(green_w, red_w)
-    weight = np.maximum(weight, yellow_w)
-
-    img[:, :, 1] = np.multiply(weight, img[:, :, 1])
-
-    return img
+    return int(left > right)
 
 
-def arrow_detection(mask, debug=False):
-    img = cv.imread('template1.jpg')
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    mask = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
+def arrow_detection(img_gray, debug=False):
+    mask = cv.blur(img_gray, (1, 1))
 
-    sift = cv.xfeatures2d.SURF_create()
-    kp1, des1 = sift.detectAndCompute(gray, None)
-    kp2, des2 = sift.detectAndCompute(mask, None)
+    if debug:
+        cv.imshow('Bilateral', mask)
+        cv.waitKey(0)
 
-    print(des1)
+    kernel = np.ones((1, 1), np.uint8)
+    mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel)
 
-    # create BFMatcher object
-    # Match descriptors.
-    # Create BFMatcher and add cluster of training images. One for now.
-    bf = cv.BFMatcher(cv.NORM_L1, crossCheck=False)  # crossCheck not supported by BFMatcher
-    clusters = np.array([des1])
-    bf.add(clusters)
+    if debug:
+        cv.imshow('Morph', mask)
+        cv.waitKey(0)
 
-    # Train: Does nothing for BruteForceMatcher though.
-    bf.train()
+    contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
-    matches = bf.match(des2)
-    matches = sorted(matches, key=lambda x: x.distance)
+    test_img = np.zeros((mask.shape[0], mask.shape[1], 3), np.uint8)
+    contour_image = np.zeros((mask.shape[0], mask.shape[1], 3), np.uint8)
 
-    img3 = cv.drawMatches(gray, kp1, mask, kp2, matches, img, (255, 255, 255))
-    cv.imshow("ASD", img3)
-    cv.waitKey(0)
+    if debug:
+        cv.imshow('Contours', cv.drawContours(contour_image, contours, -1, (255, 255, 255)))
+        cv.waitKey(0)
+
+    print("=" * 8)
+
+    for c in contours:
+        approx = cv.approxPolyDP(c, 10.0, True)
+        area = cv.contourArea(c)
+        x, y, w, h = cv.boundingRect(c)
+        aspect_ratio = float(w) / h
+
+        print("Ratio: %f, Area %f, Sides: %d" % (aspect_ratio, area, len(approx)))
+
+        if (len(approx) == 3) & (area > 30) & (abs(aspect_ratio - 1) < 0.75):
+            cv.drawContours(test_img, [c], 0, (255, 255, 255), cv.FILLED)
+            x, y, width, height = cv.boundingRect(c)
+            roi = mask[y:y + height, x:x + width]
+            cv.imshow("ASD", roi)
+            cv.waitKey(0)
+
+            direction = arrow_direction(roi)
+            return direction
+
+    return -1
 
 
 def bulb_detection(mask, debug=False):
@@ -77,7 +75,7 @@ def bulb_detection(mask, debug=False):
         cv.imshow('Morph', mask)
         cv.waitKey(0)
 
-    im2, contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
     test_img = np.zeros((mask.shape[0], mask.shape[1], 3), np.uint8)
     contour_image = np.zeros((mask.shape[0], mask.shape[1], 3), np.uint8)
@@ -146,7 +144,9 @@ def detect_traffic_lights(img, x, y, x2, y2, img_name="test", ground_truth_class
         cv.imshow('YMask', masky)
         cv.waitKey(0)
 
-    arrow_detection(img, debug=debug)
+    dir_g = arrow_detection(maskg, debug=debug)
+    dir_r = arrow_detection(maskr, debug=debug)
+    dir_y = arrow_detection(masky, debug=debug)
 
     y_bulbs = bulb_detection(masky, debug=debug)
     g_bulbs = bulb_detection(maskg, debug=debug)
@@ -167,22 +167,42 @@ def detect_traffic_lights(img, x, y, x2, y2, img_name="test", ground_truth_class
 
     names = ["Red", "Yellow", "Green"]
     vals = [r_pixels, y_pixels, g_pixels]
+    directions = ["Left", "Right"]
 
     class_idx = np.argmax(vals)
     print("Classification: %s" % names[class_idx])
 
     font = cv.FONT_HERSHEY_SIMPLEX
-    fontScale = 1
+    fontScale = 1 * crop.shape[0] / 100
     thickness = 2
 
-    original = cv.putText(original, 'Ground Truth: %s' % ground_truth_class, (0, 30), font,
-                          fontScale, (0, 255, 0), thickness, cv.LINE_AA)
-    original = cv.putText(original, 'Prediction: %s' % names[class_idx], (0, 60), font,
-                          fontScale, (0, 255, 0), thickness, cv.LINE_AA)
+    scale_percent = 20  # percent of original size
+    width = int(crop.shape[1] * scale_percent)
+    height = int(crop.shape[0] * scale_percent)
+    dim = (width, height)
+    # resize image
+    resized = cv.resize(crop, dim, interpolation=cv.INTER_AREA)
+
+    resized = cv.putText(resized, 'Ground Truth: %s' % ground_truth_class, (0, 30), font,
+                         fontScale, (0, 255, 0), thickness, cv.LINE_AA)
+    resized = cv.putText(resized, 'Prediction: %s' % names[class_idx], (0, 60), font,
+                         fontScale, (0, 255, 0), thickness, cv.LINE_AA)
+
+    if dir_g >= 0:
+        resized = cv.putText(resized, 'Green Arrow: %s' % directions[dir_g], (0, 90), font,
+                             fontScale, (0, 255, 0), thickness, cv.LINE_AA)
+
+    if dir_r >= 0:
+        resized = cv.putText(resized, 'Red Arrow: %s' % directions[dir_r], (0, 90), font,
+                             fontScale, (0, 255, 0), thickness, cv.LINE_AA)
+
+    if dir_y >= 0:
+        resized = cv.putText(resized, 'Yellow Arrow: %s' % directions[dir_y], (0, 90), font,
+                             fontScale, (0, 255, 0), thickness, cv.LINE_AA)
 
     cv.imwrite("results/%s_results.png" % img_name, all_bulbs)
     cv.imwrite("results/%s_original.png" % img_name, original)
-    cv.imwrite("results/%s_crop.png" % img_name, crop)
+    cv.imwrite("results/%s_crop.png" % img_name, resized)
 
     f = open("results/%s_results.txt" % img_name, "w")
     f.write("\nPredicted Class: %s" % names[class_idx])
@@ -233,3 +253,13 @@ if __name__ == '__main__':
                           img_name="traffic_lights_7_1", ground_truth_class="Yellow")
     detect_traffic_lights(cv.imread("traffic_lights/7.jpg"), x=523, y=105, x2=569, y2=205,
                           img_name="traffic_lights_7_2", ground_truth_class="Yellow")
+
+    # traffic_lights/8.jpg
+    # Class: Yellow
+    detect_traffic_lights(cv.imread("traffic_lights/8.jpg"), x=258, y=49, x2=313, y2=209,
+                          img_name="traffic_lights_8_1", ground_truth_class="Yellow")
+
+    # traffic_lights/9.jpg
+    # Class: Red
+    detect_traffic_lights(cv.imread("traffic_lights/9.jpeg"), x=72, y=61, x2=164, y2=335,
+                          img_name="traffic_lights_9_1", ground_truth_class="Red")
